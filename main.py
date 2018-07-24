c89ded161f0c58abde9d9e2a88e1c02b8325596a
LR <q209209a@163.com>
2018-07-24 16:51:42 +0800

    增加功能: 写入数据库

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def parse_html(html):
     infos = list()
     for i in tags:
         try:
+
             # 总价
             total_price = i.find_all('div', 'money_text')[0].text
             # 购买链接
@@ -40,7 +41,7 @@ def parse_html(html):
     pass
 
 
-def report_info(infos, raw_single_price=178, report_type='print'):
+def report_info(infos, raw_single_price=178, report_type="print"):
     count = 0
     for i in infos:
         if i['raw_single_price'] > raw_single_price:
@@ -58,12 +59,25 @@ def report_info(infos, raw_single_price=178, report_type='print'):
 
 
 
-def write_db(info):
+def write_db(info={'amount': '1', 'href': 'https://www.dd373.com/buy/third-259134718.html', 'raw_single_price': 110.0, 'total_price': '20.00元'},table='test'):
     '''
     下面是info格式
   {'amount': '',  'href': 'https://www.dd373.com/buy/third-259134709.html',  'raw_single_price': 110.0, 'total_price': '10.00元'}
     '''
     # 写入sqlite数据库
+
+    # INSERT INTO test VALUES("2018-07-22","12元","www.baidu.com",18.10,"");
+    import sqlite3
+    conn=sqlite3.connect('dd373.db')
+    c=conn.cursor()
+    # todo 如果没有表,新建
+    # 如有表,则写入数据到表table (时间,总价,url,单价,数量)
+    c.execute(
+        f"INSERT INTO {table} VALUES ('{time.ctime()}','{info['total_price']}','{info['href']}','{info['raw_single_price']}','{info['amount']}');"
+    )
+    # 提交
+    conn.commit()
+    conn.close()
 
 
 
@@ -98,28 +112,27 @@ def main():
         info=price_parse(infos)
 
         write_db(info)
-
+        print(f"{time.ctime()}, writing database...")
         time.sleep(60)
 
-def main2():
+def main2(target_price_gold=140,target_price_manao=50):
     while True:
-        # 监测玩具城金币价格>1:155的
+        # 监测玩具城金币价格>1:target_price_gold的
         print('金币价格',end='')
         target_url = 'https://www.dd373.com/s/1xj2qx-wjm3vp-r9xvef-0-0-0-tr1r70-0-0-0-0-0-0-0-0.html'
         html = get_html(target_url)
         infos = parse_html(html)
-        report_info(infos,raw_single_price=155)
+        report_info(infos,raw_single_price=target_price_gold)
 
 
 
 
-        # 监测玩具城玛瑙价格>1:54的
+        # 监测玩具城玛瑙价格>1:50的
         print('玛瑙价格',end='')
         target_url = 'https://www.dd373.com/s/1xj2qx-wjm3vp-r9xvef-0-0-0-knrc07-0-0-0-0-0-0-0-0.html'
         html = get_html(target_url)
         infos = parse_html(html)
-        report_info(infos,raw_single_price=53)
-
+        report_info(infos,raw_single_price=target_price_manao)
         time.sleep(60)
         pass
 
@@ -133,4 +146,6 @@ if __name__ == '__main__':
     # 开始运行:
     # itchat.auto_login(hotReload=True) # todo 这里要做成class内置
     # itchat.send('DD373爬虫开始运行!', toUserName='filehelper')
-    main()
+    print('开始运行!')
+    main2(target_price_gold=140,target_price_manao=55)
+
